31aac2e56049d1c3e3c27da9dbddf9cf87e902be
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-12-23 07:10:15 +0000

    Fix bug

--- biobot/translations.py
+++ biobot/translations.py
@@ -5,7 +5,8 @@ with open("translations.json") as file:
     translations = json.load(file)
 
 async def translate(message, key):
-    ret = translations.get((await message.get_sender()).lang_code, {}).get(key, None)
+    sender = getattr(message, "get_sender", getattr(message, "get_user", None))
+    ret = translations.get((await sender()).lang_code, {}).get(key, None)
     if ret is None:
         ret = translations["en"].get(key, "missing translation for {}".format(key))
     return ret
