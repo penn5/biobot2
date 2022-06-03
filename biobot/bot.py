3d4d7317ff9baa9904a646f29fe6a8ec5e48961a
Hackintosh 5 <git@hack5.dev>
2022-06-03 15:48:19 +0100

    Add feature to fetch logs from Telegram

--- biobot/bot.py
+++ biobot/bot.py
@@ -19,7 +19,7 @@ import telethon
 import io
 import re
 from telethon.tl.custom.button import Button
-from . import core
+from . import core, log
 from .translations import tr
 from .backends.bot import BotBackend
 import logging
@@ -27,6 +27,7 @@ import time
 import networkx
 import base64
 import datetime
+import grapheme
 
 logger = logging.getLogger(__name__)
 
@@ -132,6 +133,10 @@ class BioBot:
                                       telethon.events.NewMessage(pattern=fr"{start}(?:perma)?link{eoc}(?:{data} )?{username}"))
         self.client.add_event_handler(self.start_command,
                                       telethon.events.NewMessage(pattern=fr"{start}start{eoc}(.*)"))
+        self.client.add_event_handler(self.logs_command,
+                                      telethon.events.NewMessage(pattern=fr"{start}logs{eoc}(\d*)"))
+        self.client.add_event_handler(self.log_capacity_command,
+                                      telethon.events.NewMessage(pattern=fr"{start}log_capacity{eoc}(\d*)"))
         self.client.add_event_handler(self.user_joined_admission,
                                       telethon.events.ChatAction(chats=self.admissions_group))
         self.client.add_event_handler(self.user_joined_main,
@@ -155,10 +160,7 @@ class BioBot:
         new = await event.reply(await tr(event, "please_wait"))
         graph, chain = await core.get_chain(self.target, await self._select_backend(event, error=new))
         data = await self._store_data(graph)
-        await send(new, (await tr(event, "chain_format")).format(len(chain), data,
-                                                                 (await tr(event, "chain_delim")).join(user
-                                                                                                       for user in
-                                                                                                       await _format_user(chain, graph, False))))
+        await send(new, (await tr(event, "chain_format")).format(f"#chain_{len(chain)} {data}", (await tr(event, "chain_delim")).join(user for user in await _format_user(chain, graph, False))))
 
     @error_handler
     @protected
@@ -192,8 +194,7 @@ class BioBot:
         graph, diff = await core.get_diff(backend, await self._select_backend(event, 1, error=new), await tr(event, "diff_username_delim"), "\n")
         data = await self._store_data(graph)
         old_only_edges, new_only_edges, uid_edges, username_edges, old_only_names, new_only_names = diff
-        await send(new, (await tr(event, "diff_format")).format(data, new_only_names, old_only_names, username_edges,
-                                                                uid_edges, new_only_edges, old_only_edges), link_preview=False)
+        await send(new, (await tr(event, "diff_format")).format(data, new_only_names, old_only_names, username_edges, uid_edges, new_only_edges, old_only_edges))
 
     @error_handler
     @protected
@@ -241,7 +242,7 @@ class BioBot:
         if msg:
             if msg.startswith("invt"):
                 encoded = msg[12:] + "=" * (-len(msg) % 4)
-                unescaped = base64.urlsafe_b64decode(encoded.encode("utf-8")).decode("utf-8")
+                unescaped = "https://" + base64.urlsafe_b64decode(encoded.encode("utf-8")).decode("utf-8")
                 await event.respond((await tr(event, "invite_format")).format(unescaped), link_preview=False)
                 await self.client.delete_messages(self.admissions_entity.id, int(msg[4:12], 16))
                 return
@@ -258,6 +259,78 @@ class BioBot:
                     await event.respond(await tr(event, "username_help"), buttons=buttons)
                     return
         await event.respond((await tr(event, "pm_start")).format(self.target))
+
+    @error_handler
+    async def logs_command(self, event):
+        if event.sender_id in self.sudo_users:
+            level = int(event.pattern_match[1] or 0)
+            entries = log.getMemoryHandler().dumps(level)
+            if entries:
+                logs = (
+"<!DOCTYPE html>"
+"<html>"
+  "<head>"
+    "<style>"
+      # adapted from https://stackoverflow.com/a/41309213/5509575, CC BY-SA by Rounin
+      + (
+      "pre {"
+        "white-space: pre-wrap;"
+      "}"
+
+      "pre::before {"
+        "counter-reset: listing;"
+      "}"
+
+      "code {"
+        "counter-increment: listing;"
+        "text-align: left;"
+        "float: left;"
+        "clear: left;"
+        "margin-left: 4em;"
+        "margin-bottom: 1em;"
+      "}"
+
+      "code::before {"
+        "content: counter(listing) \". \";"
+        "display: block;"
+        "float: left;"
+        "text-align: right;"
+        "width: 4em;"
+        "margin-left: -4em;"
+      "}"
+
+      "code > br {"
+        "display: none;"
+      "}"
+      ).replace(": ", ":").replace(" {", "{") +
+    "</style>"
+  "</head>"
+  "<body>"
+    "<pre class=\"code\">\n"
+      + "\n<br>".join(entries) +
+    "\n</pre>"
+  "</body>"
+"</html>"
+).encode("utf-8")
+                file = io.BytesIO(logs)
+                file.name = "logs.html"
+                await self.client.send_message(event.sender_id, file=file)
+                resp = "logs_sent"
+            else:
+                resp = "logs_empty"
+        else:
+            resp = "logs_forbidden"
+        await send(event, await tr(event, resp))
+
+    @error_handler
+    async def log_capacity_command(self, event):
+        if event.sender_id in self.sudo_users:
+            capacity = int(event.pattern_match[1])
+            entries = log.getMemoryHandler().setCapacity(capacity)
+            resp = "logs_capacity_updated"
+        else:
+            resp = "logs_forbidden"
+        await send(event, await tr(event, resp))
 
     @error_handler
     async def user_joined_admission(self, event):
@@ -500,18 +573,86 @@ def _move_entities(text, entities, move=0):
     return current_text, text, entities, length
 
 
+def split(text, entities, length=4096, split_on=("\n", " "), min_length=1):
+    """
+    Split the message into smaller messages.
+    A grapheme will never be broken. Entities will be displaced to match the right location. No inputs will be mutated.
+    The end of each message except the last one is stripped of characters from [split_on]
+    :param text: the plain text input
+    :param entities: the entities
+    :param length: the maximum length of a single message
+    :param split_on: characters (or strings) which are preferred for a message break
+    :param min_length: ignore any matches on [split_on] strings before this number of characters into each message
+    :return:
+    """
+    encoded = text.encode("utf-16le")
+    pending_entities = entities
+    text_offset = 0
+    bytes_offset = 0
+    text_length = len(text)
+    bytes_length = len(encoded)
+    while text_offset < text_length:
+        if bytes_offset + length * 2 >= bytes_length:
+            yield text[text_offset:], pending_entities
+            break
+        codepoint_count = len(encoded[bytes_offset:bytes_offset + length * 2].decode("utf-16le", errors="ignore"))
+        for search in split_on:
+            search_index = text.rfind(search, text_offset + min_length, text_offset + codepoint_count)
+            if search_index != -1:
+                break
+        else:
+            search_index = text_offset + codepoint_count
+        split_index = grapheme.safe_split_index(text, search_index)
+        assert split_index > text_offset
+        split_offset_utf16 = (len(text[text_offset:split_index].encode("utf-16le"))) // 2
+        exclude = 0
+        while split_index + exclude < text_length and text[split_index + exclude] in split_on:
+            exclude += 1
+        current_entities = []
+        entities = pending_entities.copy()
+        pending_entities = []
+        for entity in entities:
+            if entity.offset < split_offset_utf16 and entity.offset + entity.length > split_offset_utf16 + exclude:
+                # spans boundary
+                current_entities.append(_copy_tl(entity, length=split_offset_utf16 - entity.offset))
+                pending_entities.append(_copy_tl(entity, offset=0, length=entity.offset + entity.length - split_offset_utf16 - exclude))
+            elif entity.offset < split_offset_utf16 < entity.offset + entity.length:
+                # overlaps boundary
+                current_entities.append(_copy_tl(entity, length=split_offset_utf16 - entity.offset))
+            elif entity.offset < split_offset_utf16:
+                # wholly left
+                current_entities.append(entity)
+            elif entity.offset + entity.length > split_offset_utf16 + exclude > entity.offset:
+                # overlaps right boundary
+                pending_entities.append(_copy_tl(entity, offset=0, length=entity.offset + entity.length - split_offset_utf16 - exclude))
+            elif entity.offset + entity.length > split_offset_utf16 + exclude:
+                # wholly right
+                pending_entities.append(_copy_tl(entity, offset=entity.offset - split_offset_utf16 - exclude))
+            else:
+                assert entity.length <= exclude
+                # ignore entity in whitespace
+        current_text = text[text_offset:split_index]
+        yield current_text, current_entities
+        text_offset = split_index + exclude
+        bytes_offset += len(current_text.encode("utf-16le"))
+        assert bytes_offset % 2 == 0
+
+
+def _copy_tl(o, **kwargs):
+    d = o.to_dict()
+    del d['_']
+    d.update(kwargs)
+    return o.__class__(**d)
+
+
 async def send(message, text, **kwargs):
     text, entities = telethon.extensions.html.parse(text)
-    entities.sort(key=lambda e: e.offset)
+    messages = split(text, entities)
     if message.out:
-        current_text, text, entities, length = _move_entities(text, entities)
+        current_text, current_entities = next(messages)
         try:
-            await message.edit(current_text, **kwargs)
+            await message.edit(current_text, entities=current_entities, **kwargs)
         except telethon.errors.rpcerrorlist.MessageNotModifiedError:
             pass
-        current_text, text, entities, length = _move_entities(text, entities, length)
-    else:
-        message = await message.reply("…", silent=True)
-    while current_text:
-        await (await message.reply("…", silent=True)).edit(current_text, **kwargs)
-        current_text, text, entities, length = _move_entities(text, entities, length)
+    for current_text, current_entities in messages:
+        message = await message.reply(current_text, formatting_entities=current_entities, silent=True, **kwargs)
